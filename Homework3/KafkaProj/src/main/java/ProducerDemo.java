6e45732fc641d5109e620cc67e9cb3acabf9f36d
chikaishiro <gran_turismo@126.com>
2018-11-19 23:42:14 +0800

    Consumer&Producer

---
+++ Homework3/KafkaProj/src/main/java/ProducerDemo.java
@@ -0,0 +1,36 @@
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+public class ProducerDemo {
+
+    public static void main(String[] args){
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("acks", "all");
+        properties.put("retries", 0);
+        properties.put("batch.size", 16384);
+        properties.put("linger.ms", 1);
+        properties.put("buffer.memory", 33554432);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = null;
+        try {
+            producer = new KafkaProducer<String, String>(properties);
+
+            String msg = "Producer demo test!";
+            producer.send(new ProducerRecord<String, String>("test", msg));
+            System.out.println("Sent:" + msg);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            producer.close();
+        }
+
+    }
+}
---
+++ Homework3/Task B-Producer&Consumer.md
@@ -0,0 +1,23 @@
+# Simple Kafka Producer & Consumer Demo(in Java)
+# Preparation 
+First, we create a topic named 'test'.  
+Then we start the zookeeper server and the Kafka server.
+# Producer Demo
+Using the Apache Kafka Java Client API, we have to modify the pom.xml, adding  
+ <dependency>  
+      <groupId>org.apache.kafka</groupId>  
+      <artifactId>kafka-clients</artifactId>  
+      <version>0.11.0.0</version>  
+  </dependency>  
+Then create ProducerDemo class, setting some important parameters:   
+properties.put("bootstrap.servers", "192.168.1.110:9092");    
+and send a message to the topic test:  
+String msg = "Producer demo test!";  
+producer.send(new ProducerRecord<String, String>("test", msg));  
+And we get message with the consumer:  
+![Image 1](/Homework3/pics/ptest.jpg)
+# Consumer Demo
+We create ConsumerDemo class, setting some important parameters:  
+kafkaConsumer.subscribe(Arrays.asList("test"));  
+And we get all the message from the topic 'test':  
+![Image 1](/Homework3/pics/ctest.jpg)
